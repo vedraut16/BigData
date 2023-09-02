d87feacc39f4f48b65eeab608932112b212be448
VED <vedraut167@gmail.com>
2023-09-02 22:19:28 +0530

    if-else

---
+++ Python_string.py
@@ -0,0 +1,24 @@
+
+# how to write multiline string
+a = '''hey
+theere
+    hows you'''
+
+# how to print the each character from the string
+s = "hello world "
+for str in s:
+  print(s[0])
+# convert string into lower case
+print(s.lower())
+
+# convert string into upper case
+print(s.upper())
+
+
+# How to use If-Else in Python
+x = 10
+y = 5
+if x==y:
+    print("numbers are eqaul")
+else:
+    print("not eqal")
---
+++ Python_while_loop.py
@@ -0,0 +1,12 @@
+# Syntax
+# while exp:
+#     statements
+
+# write a program to print the table of 9
+num = 9
+
+couter =1
+while couter <=10:
+    ans = num * couter
+    couter+=1
+    print(ans)
