ffd923fa2dbd16995d98000d646dda91f0e439fa
VED <vedraut167@gmail.com>
2023-09-02 14:33:59 +0530

    for loop practice

---
+++ Python_forLoop.py
@@ -0,0 +1,22 @@
+# Write a code to print numbers from 1 to 10
+for num in range(1,11): #rang(1,11) -> [1,2,3,4,5,6,7,8,9,10]
+    print(num)
+
+
+# Write a code to print numbers from 1 to 10 using 2 steps
+#  OR
+# Example to print odd numbers starting from value 1
+for num2 in range(1,11,2):
+    print(num2)
+
+# Write a program to print numbers from 10 to 1
+for num3 in range (10,0,-1):
+    print( "-",num3)
+# Write a program to calculate the sum of given list elements using for loop
+# output = 25
+int_list = [4,8,-2,10,5]
+sum=0
+for i in int_list:
+    sum+=i
+print(sum)
+
